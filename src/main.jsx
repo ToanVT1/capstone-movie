87bccfc6a37542d21cf5e3ac9223e21019e95999
toan <thanhtoan12x3clc@gmail.com>
2024-07-25 21:10:41 +0700

    set up Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+//Router
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
+
+
+
+//css
 import "./config/style/modern-normalize.css"
 import "./config/style/tailwind.css"
 import "./config/style/font-family.css"
 import "./config/style/main.css"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
     <App />
+
+    </RouterProvider>
+   
   </React.StrictMode>,
 )
